3bca222c36ea5b3532cdbbf736d41e47b494cfad
samuele301197 <parisisamu32@gmail.com>
2025-05-31 00:31:36 +0200

    feat:footer

--- components/AppFooter.jsx
+++ components/AppFooter.jsx
@@ -3,7 +3,78 @@
 function AppFooter() {
     return (
         <>
-        </>
+    <div className="footer">
+        <div className="container">
+            <div className="py-5">
+            <div className="col-3 flex">
+               <div> 
+                <h3>ciao</h3>
+                <ul>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                    </ul>
+                    <h3>ciao</h3>
+                    <ul>
+                        <li>item</li>
+                        <li>item</li>
+                    </ul>
+                </div>
+               <div>
+                <h3>ciao</h3>
+                <ul>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                    </ul>
+               </div>
+               <div>
+                <h3>ciao</h3>
+                <ul>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                    </ul>
+               </div>
+            </div>
+            </div>
+            <div className="col-7">
+
+            </div>
+        </div>
+       <div className="gray">
+         <div className="container py-5">
+            <div className="py-5">
+                <div className="flex">
+                    <div><button className="btn">SIGN-UP NOW</button></div>
+                    <div className="flex">
+                        <a href="">FOLLOW US</a>
+                        <img src="/img/footer-facebook.png" alt="facebook" />
+                        <img src="/img/footer-periscope.png" alt="periscope" />
+                        <img src="/img/footer-pinterest.png" alt="pinterest" />
+                        <img src="/img/footer-twitter.png" alt="twitter" />
+                        <img src="/img/footer-youtube.png" alt="youtube" />
+                    </div>
+
+                </div>
+                </div>
+        </div>      </div> 
+    </div>
+    </>
     )
 }
 
